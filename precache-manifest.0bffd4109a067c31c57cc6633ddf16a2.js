31307e8721902e49a21e097282a768b90a5c7846
Kisik Misik <700mmr@gmail.com>
2020-09-12 23:00:57 +0200

    Updates

---
+++ precache-manifest.0bffd4109a067c31c57cc6633ddf16a2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9088b891b05c80883ebf7ab2a9c679",
+    "url": "/Landing--WebCV/index.html"
+  },
+  {
+    "revision": "c1b06884b346f678cc48",
+    "url": "/Landing--WebCV/static/css/main.9f1d2035.chunk.css"
+  },
+  {
+    "revision": "5f7fa6628a144d7b1896",
+    "url": "/Landing--WebCV/static/js/2.0e62f0a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Landing--WebCV/static/js/2.0e62f0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b06884b346f678cc48",
+    "url": "/Landing--WebCV/static/js/main.ee1d6fcf.chunk.js"
+  },
+  {
+    "revision": "1f548a24b5410f16ead9",
+    "url": "/Landing--WebCV/static/js/runtime-main.a967a8c7.js"
+  },
+  {
+    "revision": "aa46d234cf405126cdd1c36600e4b978",
+    "url": "/Landing--WebCV/static/media/FontsFree-Net-Proxima-Nova-Black.aa46d234.otf"
+  },
+  {
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902",
+    "url": "/Landing--WebCV/static/media/FontsFree-Net-Proxima-Nova-Bold.72b6f2c9.otf"
+  },
+  {
+    "revision": "ac51ad8a36d5bab6da11d454bcd06561",
+    "url": "/Landing--WebCV/static/media/FontsFree-Net-Proxima-Nova-Light.ac51ad8a.otf"
+  },
+  {
+    "revision": "7a66caadc12a9a17c103e9d930694e4d",
+    "url": "/Landing--WebCV/static/media/arrow.7a66caad.svg"
+  },
+  {
+    "revision": "cc12314ed6405fdbc9029fc687e35398",
+    "url": "/Landing--WebCV/static/media/circle-blue.cc12314e.svg"
+  },
+  {
+    "revision": "7ef0d73d026e3e416a8e21819595df7a",
+    "url": "/Landing--WebCV/static/media/circle.7ef0d73d.svg"
+  },
+  {
+    "revision": "6180538d8f869d496c165667fd69aeaf",
+    "url": "/Landing--WebCV/static/media/github.6180538d.svg"
+  },
+  {
+    "revision": "64dc861ce467a23bf413debddedba9f7",
+    "url": "/Landing--WebCV/static/media/linkedin.64dc861c.svg"
+  },
+  {
+    "revision": "48bda8f3e7cd432b70b74d19b76a6204",
+    "url": "/Landing--WebCV/static/media/mail.48bda8f3.svg"
+  },
+  {
+    "revision": "932a26f199fb858e8d7fd80480cb6fc9",
+    "url": "/Landing--WebCV/static/media/map.932a26f1.png"
+  },
+  {
+    "revision": "6ea34720ada60953028b8a0ce2605dfd",
+    "url": "/Landing--WebCV/static/media/me2.6ea34720.png"
+  },
+  {
+    "revision": "cf3d01268f4e4b355b1b89fa7fbf0133",
+    "url": "/Landing--WebCV/static/media/menu.cf3d0126.svg"
+  },
+  {
+    "revision": "b0a2ba0c33b318db69221539758cf21f",
+    "url": "/Landing--WebCV/static/media/phone.b0a2ba0c.svg"
+  },
+  {
+    "revision": "3d0c5526d30e8b6b889b08d07d708ead",
+    "url": "/Landing--WebCV/static/media/pin.3d0c5526.svg"
+  },
+  {
+    "revision": "e0d0de8005cfafff72f6f4908cd1d89a",
+    "url": "/Landing--WebCV/static/media/tick-green.e0d0de80.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ef79ef714a3685abd2b1f770b2b8d13.js"
+  "/Landing--WebCV/precache-manifest.0bffd4109a067c31c57cc6633ddf16a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Landing--WebCV/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
