d4058d9c240abb7c0a822884a28480cd2ed27bab
Kisik Misik <700mmr@gmail.com>
2020-09-18 15:33:00 +0200

    Updates

---
+++ precache-manifest.2dac898643509c299cc4c385a0984c34.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f536aa08ab4def24a986834938dd8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66c72bd46037d0d60bfa",
+    "url": "/static/css/main.e393baff.chunk.css"
+  },
+  {
+    "revision": "95813bcdf18c4db5b6db",
+    "url": "/static/js/2.d3c75d6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3c75d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66c72bd46037d0d60bfa",
+    "url": "/static/js/main.6b6465a0.chunk.js"
+  },
+  {
+    "revision": "29a768c22cd0909331d2",
+    "url": "/static/js/runtime-main.e5a428e6.js"
+  },
+  {
+    "revision": "aa46d234cf405126cdd1c36600e4b978",
+    "url": "/static/media/FontsFree-Net-Proxima-Nova-Black.aa46d234.otf"
+  },
+  {
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902",
+    "url": "/static/media/FontsFree-Net-Proxima-Nova-Bold.72b6f2c9.otf"
+  },
+  {
+    "revision": "ac51ad8a36d5bab6da11d454bcd06561",
+    "url": "/static/media/FontsFree-Net-Proxima-Nova-Light.ac51ad8a.otf"
+  },
+  {
+    "revision": "7a66caadc12a9a17c103e9d930694e4d",
+    "url": "/static/media/arrow.7a66caad.svg"
+  },
+  {
+    "revision": "7ef0d73d026e3e416a8e21819595df7a",
+    "url": "/static/media/circle.7ef0d73d.svg"
+  },
+  {
+    "revision": "6180538d8f869d496c165667fd69aeaf",
+    "url": "/static/media/github.6180538d.svg"
+  },
+  {
+    "revision": "64dc861ce467a23bf413debddedba9f7",
+    "url": "/static/media/linkedin.64dc861c.svg"
+  },
+  {
+    "revision": "48bda8f3e7cd432b70b74d19b76a6204",
+    "url": "/static/media/mail.48bda8f3.svg"
+  },
+  {
+    "revision": "932a26f199fb858e8d7fd80480cb6fc9",
+    "url": "/static/media/map.932a26f1.png"
+  },
+  {
+    "revision": "6ea34720ada60953028b8a0ce2605dfd",
+    "url": "/static/media/me2.6ea34720.png"
+  },
+  {
+    "revision": "cf3d01268f4e4b355b1b89fa7fbf0133",
+    "url": "/static/media/menu.cf3d0126.svg"
+  },
+  {
+    "revision": "b0a2ba0c33b318db69221539758cf21f",
+    "url": "/static/media/phone.b0a2ba0c.svg"
+  },
+  {
+    "revision": "3d0c5526d30e8b6b889b08d07d708ead",
+    "url": "/static/media/pin.3d0c5526.svg"
+  },
+  {
+    "revision": "3a5afd7357c0a2c43162c69e03cf7d5a",
+    "url": "/static/media/tick-black.3a5afd73.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Landing--WebCV/precache-manifest.596d52c1fc8d3f98ca8d3734e9c74341.js"
+  "/precache-manifest.2dac898643509c299cc4c385a0984c34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Landing--WebCV/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
