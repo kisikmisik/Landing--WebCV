fc38ef0a8dc24c5e72c3071f64aee3ff3b7b6234
Kisik Misik <700mmr@gmail.com>
2020-09-20 18:51:56 +0200

    emailjs set

--- src/components/Sliders/ContactSlide/ContactPopup/ContactPopup.jsx
+++ src/components/Sliders/ContactSlide/ContactPopup/ContactPopup.jsx
@@ -1,48 +1,114 @@
 import React, {useState} from "react";
 import styles from "./ContactPopup.scss"
 import {useSpring, animated} from 'react-spring';
+import * as emailjs from 'emailjs-com'
+import { Button, FormFeedback, Form, FormGroup, Label, Input} from 'reactstrap'
 
-let ContactPopup = (props) => {
-    let [isMessageSent, changeMessageStatus] = useState(false)
-    let handleSendClick = (evt) => {
-        changeMessageStatus(true);
+class ContactPopup extends React.Component {
+    state = {
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+        isMessageSent: false,
+        isErrorOccured: false,
+        isPending: false
     }
-    const animProps = useSpring({left: 65, transform: "scale(1)",
-        from: {left: 400, transform: "scale(0.2)"}, config: {duration: 350}})
-
-
-
-    return (
-        <animated.form style={animProps} id="contact__form" className="contact-popup">
-            <div className="contact-popup__wrapper">
-                <label className="contact-popup__input-wrapper">
-                    <p className="contact-popup__label">Your email:</p>
-                    <input type="email"
-                           className="contact-popup__email"
-                           placeholder="Email address"
-                           autoFocus={true}
-                           required={true}
-                           maxLength="50"/>
-                </label>
-                <label className="contact-popup__input-wrapper">
-                    <p className="contact-popup__label">Title:</p>
-                    <input type="text" className="contact-popup__name" placeholder="Theme" maxLength="50" required={true}/>
-                </label>
-                <label className="contact-popup__input-wrapper">
-                    <p className="contact-popup__label">Message:</p>
-                    <textarea cols="30" rows="5"
-                              className="contact-popup__message"
-                              placeholder="Type your message.."
-                              maxLength="200"
-                              required={true}/>
-                </label>
-            </div>
-            {isMessageSent ?
-                <p className="contact-popup__sent">Thanks! I'll answer shortly.</p> :
-                <button onClick={handleSendClick} type="submit" className="contact-popup__send">Send message</button>
+    handleSubmit(e) {
+        this.setState({isMessageSent: true})
+        this.setState({isPending: true})
+        e.preventDefault()
+        const { name, email, subject, message } = this.state
+        let templateParams = {
+            from_name: email,
+            to_name: 'kisikMisik',
+            subject: subject,
+            message_html: message,
+        }
+        emailjs.send(
+            '1133',
+            'template_jqqon65',
+            templateParams,
+            'user_PN0oibNSmYBjB8S1N6jqO'
+        ).then(response => {
+            this.setState({isPending: false})
+            if (response.status === 200) {
+                this.resetForm()
             }
-        </animated.form>
-    )
+        }).catch (response => {
+            this.setState({isPending: false})
+            this.setState({isErrorOccured: true})
+        })
+       
+    }
+    resetForm() {
+        this.setState({isMessageSent: true})
+        this.setState({
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        })
+    }
 
+    handleChange = (param, e) => {
+        this.setState({ [param]: e.target.value })
+      }
+
+    render() {
+        return (
+            <Form onSubmit={this.handleSubmit.bind(this)} id="contact__form" className="contact-popup">
+                <div className="contact-popup__wrapper">
+                <FormGroup controlId="formBasicEmail">
+                    <Label className="contact-popup__input-wrapper">
+                        <p className="contact-popup__label">Your email:</p>
+                        <Input type="email"
+                               className="contact-popup__email"
+                               placeholder="Email address"
+                               autoFocus={true}
+                               required={true}
+                               value={this.state.email}
+                               onChange={this.handleChange.bind(this, "email")}
+                               maxLength="50"/>
+                    </Label>
+                </FormGroup>
+                <FormGroup controlId="formBasicSubject">
+                    <Label className="contact-popup__input-wrapper">
+                        <p className="contact-popup__label">Title:</p>
+                        <Input type="text" 
+                                className="contact-popup__name" 
+                                placeholder="Theme" 
+                                maxLength="50"
+                                required={true}
+                                value={this.state.subject}
+                                onChange={this.handleChange.bind(this, "subject")}
+                                />
+                    </Label>   
+                </FormGroup> 
+                <FormGroup controlId="formBasicMessage">
+                    <Label className="contact-popup__input-wrapper">
+                        <p className="contact-popup__label">Message:</p>
+                        <textarea cols="30" rows="5"
+                                  className="contact-popup__message"
+                                  placeholder="Type your message.."
+                                  maxLength="200"
+                                  required={true}
+                                  value={this.state.message}
+                                  onChange={this.handleChange.bind(this, "message")}/>
+                    </Label>
+                </FormGroup>
+                    
+                </div>
+            
+                {this.state.isMessageSent && !this.state.isErrorOccured && !this.state.isPending &&
+                    <p className="contact-popup__sent">Thanks! I'll answer shortly.</p>}
+                {!this.state.isMessageSent && <button  type="submit" className="contact-popup__send">Send message</button>}
+                {this.state.isErrorOccured && <p className="contact-popup__error">Error. Try using my contact info.</p>}
+                {this.state.isPending && <p className="contact-popup__pending">Sending..</p>}
+            </Form>
+        )
+
+    }
 }
+
 export default ContactPopup;
--- src/components/Sliders/HireSlide/HireSlide.jsx
+++ src/components/Sliders/HireSlide/HireSlide.jsx
@@ -1,50 +1,138 @@
 import React from "react";
 import styles from "./HireSlide.scss"
+import * as emailjs from 'emailjs-com'
+import { Button, FormFeedback, Form, FormGroup, Label, Input} from 'reactstrap'
 
-let HireSlide = (props) => {
-    return (
-        <section id='slide-4' className="hire-me slide slide--4">
-            <h2 className="visually-hidden">Hire me to make a website or webapp for you or your business</h2>
-            <form className="hire-me__form">
-                <p className="hire-me__question">You want me to do</p>
-                <div className="hire-me__work-type-wrapper">
-                    <label className="hire-me__work hire-me__work--landing button">
-                        <input type="checkbox" className="visually-hidden"/>
-                        <span className="hire-me__work-text">Landing page</span>
-                    </label>
-                    <label className="hire-me__work hire-me__work--shop button">
-                        <input type="checkbox" className="visually-hidden"/>
-                        <span className="hire-me__work-text">Online shop</span>
-                    </label>
-                    <label className="hire-me__work hire-me__work--html button">
-                        <input type="checkbox" className="visually-hidden"/>
-                        <span className="hire-me__work-text">Semantic HTML</span>
-                    </label>
-                    <label className="hire-me__work hire-me__work--app button">
-                        <input type="checkbox" className="visually-hidden"/>
-                        <span className="hire-me__work-text">Web application</span>
-                    </label>
-                    <label className="hire-me__work hire-me__work--design button">
-                        <input type="checkbox" className="visually-hidden"/>
-                        <span className="hire-me__work-text">UI/UX design</span>
-                    </label>
-                    <label className="hire-me__work hire-me__work--job button">
-                        <input type="checkbox" className="visually-hidden"/>
-                        <span className="hire-me__work-text">Remote ft/pt job</span>
-                    </label>
-                    <label className="hire-me__work hire-me__work--other button">
-                        <input type="checkbox" className="visually-hidden"/>
-                        <span className="hire-me__work-text">Other</span>
-                    </label>
-                </div>
-                <div className="hire-me__bottom-wrapper">
-                    <input type="text" className="hire-me__name" placeholder="Name" maxLength="50"/>
-                    <input type="email" className="hire-me__email" placeholder="Email" maxLength="50"/>
-                </div>
-                <button type="submit" className="hire-me__submit">Send request</button>
-            </form>
-        </section>
-    )
+class HireSlide extends React.Component {
+    state = {
+        name: '',
+        email: '',
+        landing: false,
+        shop: false,
+        html: false,
+        app: false,
+        design: false,
+        job: false,
+        other: false,
+        isMessageSent: false,
+        isErrorOccured: false,
+        isPending: false
+    }
+    handleSubmit(e) {
+        this.setState({isMessageSent: true})
+        this.setState({isPending: true})
+        e.preventDefault()
+        
+        let templateParams = {
+            from_name: this.state.name,
+            from_email: this.state.email,
+            to_name: 'kisikMisik',
+            landing: this.state.landing,
+            shop: this.state.shop,
+            html: this.state.html,
+            app: this.state.app,
+            design: this.state.design,
+            job: this.state.job,
+            other: this.state.other
+        }
+        emailjs.send(
+            '1133',
+            'template_hvpnj0l',
+            templateParams,
+            'user_PN0oibNSmYBjB8S1N6jqO'
+        ).then(response => {
+            this.setState({isPending: false})
+            if (response.status === 200) {
+                this.resetForm()
+            }
+        }).catch (response => {
+            this.setState({isPending: false})
+            this.setState({isErrorOccured: true})
+        })
+       
+    }
+    resetForm() {
+        this.setState({isMessageSent: true})
+        this.setState({
+            name: '',
+            email: '',
+            landing: false,
+            shop: false,
+            html: false,
+            app: false,
+            design: false,
+            job: false,
+            other: false 
+        })
+    }
+
+    handleChange = (param, e) => {
+        this.setState({ [param]: e.target.value })
+      }
+
+    render () {
+        return (
+            <section id='slide-4' className="hire-me slide slide--4">
+                <h2 className="visually-hidden">Hire me to make a website or webapp for you or your business</h2>
+                <form onSubmit={this.handleSubmit.bind(this)} className="hire-me__form">
+                    <p className="hire-me__question">You want me to do</p>
+                    <div className="hire-me__work-type-wrapper">
+                        <label className="hire-me__work hire-me__work--landing button">
+                            <input checked={this.state.landing} onChange={this.handleChange.bind(this, "landing")} type="checkbox" className="visually-hidden"/>
+                            <span className="hire-me__work-text">Landing page</span>
+                        </label>
+                        <label className="hire-me__work hire-me__work--shop button">
+                            <input checked={this.state.shop} onChange={this.handleChange.bind(this, "shop")} type="checkbox" className="visually-hidden"/>
+                            <span className="hire-me__work-text">Online shop</span>
+                        </label>
+                        <label className="hire-me__work hire-me__work--html button">
+                            <input checked={this.state.html} onChange={this.handleChange.bind(this, "html")} type="checkbox" className="visually-hidden"/>
+                            <span className="hire-me__work-text">Semantic HTML</span>
+                        </label>
+                        <label className="hire-me__work hire-me__work--app button">
+                            <input checked={this.state.app} onChange={this.handleChange.bind(this, "app")} type="checkbox" className="visually-hidden"/>
+                            <span className="hire-me__work-text">Web application</span>
+                        </label>
+                        <label className="hire-me__work hire-me__work--design button">
+                            <input checked={this.state.design} onChange={this.handleChange.bind(this, "design")} type="checkbox" className="visually-hidden"/>
+                            <span className="hire-me__work-text">UI/UX design</span>
+                        </label>
+                        <label className="hire-me__work hire-me__work--job button">
+                            <input checked={this.state.job} onChange={this.handleChange.bind(this, "job")} type="checkbox" className="visually-hidden"/>
+                            <span className="hire-me__work-text">Remote ft/pt job</span>
+                        </label>
+                        <label className="hire-me__work hire-me__work--other button">
+                            <input checked={this.state.other} onChange={this.handleChange.bind(this, "other")} type="checkbox" className="visually-hidden"/>
+                            <span className="hire-me__work-text">Other</span>
+                        </label>
+                    </div>
+                    <div className="hire-me__bottom-wrapper">
+                        <input value={this.state.name} 
+                                onChange={this.handleChange.bind(this, "name")} 
+                                type="text" 
+                                className="hire-me__name" 
+                                placeholder="Name" 
+                                maxLength="50" 
+                                required="true"/>
+                        <input value={this.state.email} 
+                                type="email" 
+                                className="hire-me__email" 
+                                placeholder="Email" 
+                                maxLength="50" 
+                                onChange={this.handleChange.bind(this, "email")}  
+                                required="true"/>
+                    </div>
+                    
+                    {this.state.isMessageSent && !this.state.isErrorOccured && !this.state.isPending &&
+                    <p className="hire-me__sent">Thanks! I'll answer shortly.</p>}
+                    {!this.state.isMessageSent && <button type="submit" className="hire-me__submit">Send request</button>}
+                    {this.state.isErrorOccured && <p className="hire-me__error">Error. Try using my contact info.</p>}
+                    {this.state.isPending && <p className="hire-me__pending">Sending..</p>}
+                </form>
+            </section>
+        )
+    }
+    
 }
 
 export default HireSlide
